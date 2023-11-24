3dd8a50ad34d6322b2ccb4a265142193186e7960
srinija landa <144719297+srinijalanda93@users.noreply.github.com>
2023-11-24 19:12:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 function volume_sphere() {
     //Write your code here
-  
+  let r=document.getElementById('MyForm').radius.value;
+	
+	document.getElementById('MyForm').volume.value=((4/3)*Math.PI*r*r*r).toFixed(4);
+	 event.preventDefault();
 } 
+
+window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
